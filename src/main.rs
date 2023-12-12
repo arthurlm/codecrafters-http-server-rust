59784e436eab374c7cd6bdd6bd388ed16c46d99a
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2023-12-12 14:01:39 +0100

    Better way of reading HTTP head

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{self, BufRead, BufReader, Write},
+    io::{self, BufRead, BufReader, Read, Write},
     net::{TcpListener, TcpStream},
 };
 
@@ -25,14 +25,10 @@ fn main() {
 }
 
 fn handle_client(mut stream: TcpStream) -> io::Result<()> {
-    println!("accepted new connection");
-
-    let mut buf_reader = BufReader::new(stream.try_clone()?);
-    let mut req_raw = String::with_capacity(512);
-    buf_reader.read_line(&mut req_raw)?;
+    let req_raw = read_request_head(&mut stream)?;
 
     let Ok((_, request)) = HttpRequest::parse(&req_raw) else {
-        eprintln!("Invalid HTTP request: {req_raw}");
+        eprintln!("Invalid HTTP request: {req_raw:?}");
         return Ok(());
     };
 
@@ -40,16 +36,42 @@ fn handle_client(mut stream: TcpStream) -> io::Result<()> {
         (HttpVerb::Get, target) if target.starts_with("/echo/") => {
             let response = HttpResponse::new(HttpStatusCode::Ok, &target[6..]);
             write!(stream, "{}", response.to_string())?;
+            log_response(&request, &response);
         }
         (HttpVerb::Get, "/") => {
             let response = HttpResponse::new(HttpStatusCode::Ok, ());
             write!(stream, "{}", response.to_string())?;
+            log_response(&request, &response);
         }
         _ => {
             let response = HttpResponse::new(HttpStatusCode::NotFound, ());
             write!(stream, "{}", response.to_string())?;
+            log_response(&request, &response);
         }
     }
 
     Ok(())
+}
+
+fn read_request_head<R: Read>(stream: &mut R) -> io::Result<String> {
+    let mut buf_reader = BufReader::new(stream);
+    let mut output = String::with_capacity(1024);
+
+    loop {
+        let mut line = String::with_capacity(512);
+        buf_reader.read_line(&mut line)?;
+        output.push_str(&line);
+        if line == "\r\n" {
+            break;
+        };
+    }
+
+    Ok(output)
+}
+
+fn log_response(req: &HttpRequest, res: &HttpResponse) {
+    println!(
+        "Response: {:?} '{}' => {:?}",
+        req.verb, req.target, res.code
+    );
 }
